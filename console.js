0ce1488f9d2551e6006e5e4ef6d675c71a10f29d
Ricardo Montes Carrasco <ricardomontescarrasco1@gmail.com>
2025-05-23 20:57:49 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,14 +1,4 @@
 // This is a test
-console.log("test");
-console.log("test2");
-console.log("test3");
-console.log("test4");
-console.log("test5");
-console.log("test6");
-
-console.log("test");
-console.log("test2");
-console.log("test3");
-console.log("test4");
-console.log("test5");
-console.log("test6");
+for (let i = 0; i < 80; i++) {
+  console.log("test");
+}
